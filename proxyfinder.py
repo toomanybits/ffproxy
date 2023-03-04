50ce92542ee6aeeb985380262ef2d12d7a40d182
toomanybits <124254343+toomanybits@users.noreply.github.com>
2023-03-05 00:23:59 +0530

    added csv  exporting feature for working proxies

--- proxyfinder.py
+++ proxyfinder.py
@@ -3,34 +3,60 @@
 # it also gives direct link to load
 import requests
 import json
+import csv
 proxy_url = "https://proxylist.geonode.com/api/proxy-list?limit=500&page=1&sort_by=lastChecked&sort_type=desc"
+# site to check whether proxy working returns ip dictionary
 target_url = "http://httpbin.org/ip"
+
+# function created to return proxy list from geonode json data
 
 
 def fetchproxy(url):
     data = requests.get(url)
     json_data = json.loads(data.text)
     proxies = list()
+    # adding basic format of proxy
     for i in range(0, len(json_data["data"])):
         proxy = json_data["data"][i]["ip"] + \
             str(":") + json_data["data"][i]["port"]
-        proxies += [{'http': 'http://' + proxy}]
+        proxies.append(proxy)
     return proxies
+
+# function created to ping target url and find the working one
 
 
 def send_request(proxies):
+    workingproxies = list()
     for proxy in proxies:
         try:
+            # will send request to httpbin using proxy from fetched proxies
             response = requests.get(target_url,
-                                    proxies=proxy, timeout=3)
+                                    # adding basic schema of proxy
+                                    proxies={'http': 'http://' + proxy,
+                                             'https': 'https://' + proxy},
+                                    timeout=3)
             print("Working Proxy : " + response.json()["origin"])
+            # will add working proxy to workingproxylist
+            workingproxies.append(proxy)
         except:
+            print("Failed Proxy : " + proxy)
             pass
+        return workingproxies
+
+# for export working proxies as a csv file
+
+
+def export(proxylist):
+    with open("workingproxy.csv", "w", newline="", encoding="UTF-8") as f:
+        # loop for witing each row in csv
+        for proxy in proxylist:
+            f.write(proxy + "\n")
 
 
 def main():
     proxies = fetchproxy(proxy_url)
-    send_request(proxies)
+    working = send_request(proxies)
+    export(working)
 
 
 if __name__ == "__main__":
